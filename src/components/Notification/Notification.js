dd9a814f59bd7633072a08c73e154e379e5fa999
RoxyRomanyshyn <iamroksa@icloud.com>
2020-09-09 21:44:02 +0300

    add

---
+++ src/components/Notification/Notification.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Notification({
+    title
+}) {
+    return ( <
+        >
+        <
+        p > No feedback given < /p> <
+        />
+    )
+}
